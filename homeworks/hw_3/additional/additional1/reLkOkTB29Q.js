6d0d0658048f36c81e9df764c9056f4b313ec262
yu-nykanorova <juliyasos@ukr.net>
2025-12-26 18:48:05 +0200

    added homework_3 - additional 1

---
+++ homeworks/hw_3/additional/additional1/reLkOkTB29Q.js
@@ -0,0 +1,127 @@
+// #reLkOkTB29Q
+// стоврити масив книжок (назва, кількість сторінок, автори , жанри).
+// - знайти наібльшу книжку.
+// - знайти книжку/ки з найбільшою кількістю жанрів
+// - знайти книжку/ки з найдовшою назвою
+// - знайти книжку/ки які писали 2 автори
+// - знайти книжку/ки які писав 1 автор
+
+let books = [
+    {
+        id: 1,
+        title: "To Kill a Mockingbird",
+        pagesCount: 281,
+        authors: ["Harper Lee"],
+        genres : ["Literary Fiction"]
+    },
+    {
+        id: 2,
+        title: "Good Omens",
+        pagesCount: 432,
+        authors: ["Terry Pratchett", "Neil Gaiman"],
+        genres : ["Fantasy", "Humor"]
+    },
+    {
+        id: 3,
+        title: "1984",
+        pagesCount: 328,
+        authors: ["George Orwell"],
+        genres : ["Dystopian", "Science Fiction", "Political Fiction"]
+    },
+    {
+        id: 4,
+        title: "The Hobbit",
+        pagesCount: 310,
+        authors: ["J.R.R. Tolkien"],
+        genres : ["Fantasy"]
+    },
+    {
+        id: 5,
+        title: "The Martian",
+        pagesCount: 369,
+        authors: ["Andy Weir"],
+        genres : ["Science Fiction", "Adventure"]
+    },
+    {
+        id: 6,
+        title: "The Art of War",
+        pagesCount: 273,
+        authors: ["Sun Tzu"],
+        genres : ["Philosophy", "Military Strategy"]
+    },
+    {
+        id: 7,
+        title: "The Girl with the Dragon Tattoo",
+        pagesCount: 465,
+        authors: ["Stieg Larsson"],
+        genres : ["Crime", "Thriller"]
+    },
+    {
+        id: 8,
+        title: "The Name of the Wind",
+        pagesCount: 662,
+        authors: ["Patrick Rothfuss"],
+        genres : ["Fantasy", "Coming-of-Age"]
+    },
+    {
+        id: 9,
+        title: "The Cuckoo’s Calling",
+        pagesCount: 464,
+        authors: ["Robert Galbraith (J.K. Rowling)", "Charles Smith†"],
+        genres : ["Mystery", "Crime Fiction"]
+    },
+    {
+        id: 10,
+        title: "Sapiens: A Brief History of Humankind",
+        pagesCount: 498,
+        authors: ["Yuval Noah Harari"],
+        genres : ["Nonfiction", "History", "Anthropology"]
+    }
+];
+
+const biggestBookBox = document.getElementById("biggest-book");
+const multigenreBooksBox = document.getElementById("multigenre-book");
+const longestTitleBookBox = document.getElementById("longest-title-book");
+const twoAuthorsBookBox = document.getElementById("two-authors-book");
+const singleAuthorBookBox = document.getElementById("single-author-book");
+
+let maxPagesAmount = 0;
+let maxPagesAmountBook = "";
+let maxGenresAmount = 0;
+let longestTitleBook = "";
+
+// основний цикл перебору книжок для пошуку заданих умовою задачі значень
+for (let book of books) {
+    if (book.pagesCount > maxPagesAmount) {
+        maxPagesAmount = book.pagesCount;
+        maxPagesAmountBook = book.title;
+    }
+    if (book.genres.length > maxGenresAmount) {
+        maxGenresAmount = book.genres.length;
+    }
+    if (book.title.length > longestTitleBook.length) {
+        longestTitleBook = `"${book.title}"`;
+    }
+}
+
+// найбільша книжка
+const biggestBook = `The book "${maxPagesAmountBook}" has ${maxPagesAmount} pages`;
+biggestBookBox.append(biggestBook);
+
+// мультижанрові книжки
+
+let count = 0;
+for (let book of books) {
+    if (book.genres.length === maxGenresAmount) {
+        const multigenreBook = document.createElement("p");
+        multigenreBook.innerHTML = `${count + 1}) "${book.title}" - ${book.genres.join(", ")}.`;
+        multigenreBooksBox.append(multigenreBook);
+        count++;
+    }
+}
+
+// книжка з найдовшою назвою
+longestTitleBookBox.append(longestTitleBook);
+
+
+
---
+++ homeworks/hw_3/additional/additional1/script.js
@@ -0,0 +1,92 @@
+// #qLQLJSeN7i
+// - є масив [2,17,13,6,22,31,45,66,100,-18] :
+
+let arr = [2,17,13,6,22,31,45,66,100,-18];
+
+let i = 0;
+
+// 3. перебрати циклом while та вивести  числа тільки з непарним індексом
+
+// while (i < arr.length) {
+//     if (i % 2 !== 0) {
+//         console.log(`${i} - ${arr[i]}`);
+//     }
+//     i++;
+// }
+
+// 4. перебрати циклом for та вивести  числа тільки з непарним індексом
+
+// for (i = 0; i < arr.length; i+=2) {
+//     console.log(`${i} - ${arr[i]}`);
+// }
+
+// 5. перебрати циклом while та вивести  числа тільки парні  значення
+
+// while (i < arr.length) {
+//     if (arr[i] % 2 === 0) {
+//         console.log(`${i} - ${arr[i]}`);
+//     }
+//     i++;
+// }
+
+// 6. перебрати циклом for та вивести  числа тільки парні  значення
+
+// for (i = 0; i < arr.length; i++) {
+//     if (arr[i] % 2 === 0) {
+//          console.log(`${i} - ${arr[i]}`);
+//      }
+// }
+
+// 7. замінити кожне число кратне 3 на слово "okten"
+
+// for (i = 0; i < arr.length; i++) {
+//     if (arr[i] % 3 === 0) {
+//         arr[i] = "okten";
+//     }
+// }
+// console.log(arr);
+
+// 8. вивести масив в зворотньому порядку.
+
+// let newArr = [];
+// for (let i = 0; i < arr.length; i++) {
+//     newArr[i] = arr[arr.length - 1 - i];
+// }
+// console.log(newArr);
+
+// - Створити масив з 10 елементів числового, стірчкового і булевого типу. За допомогою if та typeof вивести тільки булеві елементи
+
+let diffTypesArr = [3, false, 17, 28, "hi",true, "there", "everyone", true, false];
+
+// for (let i = 0; i < diffTypesArr.length; i++) {
+//     if ( typeof(diffTypesArr[i]) === "boolean") {
+//         console.log(`Element ${i} is ${diffTypesArr[i]}`);
+//     }
+// }
+
+// #9stMq2ou
+// - Створити масив з 10 елементів числового, стірчкового і булевого типу. За допомогою if та typeof вивести тільки числові елементи
+
+// for (let i = 0; i < diffTypesArr.length; i++) {
+//     if ( typeof(diffTypesArr[i]) === "number") {
+//         console.log(`Element ${i} is ${diffTypesArr[i]}`);
+//     }
+// }
+
+// #mK4pmM4
+// - Створити масив з 10 елементів числового, стрічкового і булевого типу. За допомогою if та typeof вивести тільки рядкові елементи
+
+// for (let i = 0; i < diffTypesArr.length; i++) {
+//     if ( typeof(diffTypesArr[i]) === "string") {
+//         console.log(`Element ${i} is ${diffTypesArr[i]}`);
+//     }
+// }
+
+
+
+
+
+
+
+
+
